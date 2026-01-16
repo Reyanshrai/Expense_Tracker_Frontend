b42b309564ea7c154cf5c38457b71d4d538134a2
reyanshrai <reyanshrai05@gmail.com>
2026-01-17 00:20:57 +0530

    Add registration and logout functionality with error handling

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,5 +1,22 @@
-import {signInWithEmailAndPassword,signOut } from 'firebase/auth';
-import {auth} from './firebase'
+import { router } from 'expo-router';
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from 'firebase/auth';
+import { auth } from './firebase';
+
+export const registerWithEmail = async (email:string,password:string,confirmPassword:string) =>{
+    try{
+        if(password !== confirmPassword){
+            throw new Error("Passwords do not match")
+        }
+        const userCredentail = await createUserWithEmailAndPassword(
+            auth,
+            email,
+            password
+        )
+        return userCredentail
+    }catch(error:any){
+        throw new Error(error.message || "SignUp Failed")
+    }
+}
 
 export const loginWithEmail = async (email : string,password : string) => {
     try{
@@ -17,6 +34,7 @@ export const loginWithEmail = async (email : string,password : string) => {
 export const logout = async () => {
     try{
         await signOut(auth)
+        router.replace("/(auth)/login")
     }catch(error:any){
         throw new Error("Logout Failed")
     }
