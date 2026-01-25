4fc5ed6688f27378ba8523fc74cc7d5a4f263fa8
reyanshrai <reyanshrai05@gmail.com>
2026-01-25 23:29:51 +0530

    Add AuthContext to manage user authentication state and loading status

---
+++ src/context/AuthContext.tsx
@@ -0,0 +1,31 @@
+import { createContext, useEffect, useState } from "react";
+import { onAuthStateChanged, User } from "firebase/auth";
+import { auth } from "../services/firebase";
+
+export const AuthContext = createContext<{
+  user: User | null;
+  loading: boolean;
+}>({
+  user: null,
+  loading: true,
+});
+
+export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
+  const [user, setUser] = useState<User | null>(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const unsub = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      setLoading(false);
+    });
+
+    return unsub;
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ user, loading }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
