f39f679067b0e68e658c3a140cac9cae2674c6e6
reyanshrai <reyanshrai05@gmail.com>
2026-02-23 10:36:37 +0530

    Add group service functions for creating groups and real-time user group listening

---
+++ src/services/group.ts
@@ -0,0 +1,36 @@
+import {
+  addDoc,
+  collection,
+  query,
+  where,
+  onSnapshot,
+  Timestamp,
+} from "firebase/firestore";
+import { db } from "@/src/services/firebase";
+import { User } from "firebase/auth";
+
+export const createGroup = async (user: User, groupName: string,memberEmails: string[] = []) => {
+  return await addDoc(collection(db, "groups"), {
+    name: groupName,
+    createdBy: user.uid,
+    members: [user.uid,],
+    memberEmails,// 🔥 later resolve emails to UIDs
+    createdAt: Timestamp.now(),
+  });
+};
+
+// Listen user Groups in real-time
+
+export const listenUserGroups = (userId: string, callback: (groups: any[]) => void) => {
+    
+
+    const q = query(
+        collection(db, "groups"), 
+        where("members", "array-contains", userId)
+    );
+
+    return onSnapshot(q, (snapshot) => {
+        const groups = snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+        callback(groups);
+    });
+}
