1437b872a38d07debccbba8a24ad442fdff6c756
reyanshrai <reyanshrai05@gmail.com>
2026-02-23 16:43:15 +0530

    Add GroupList component for rendering a list of groups with animated transitions and improved state management.

---
+++ src/components/groups/GroupList.tsx
@@ -0,0 +1,29 @@
+import { Animated, FlatList } from "react-native";
+import GroupCard from "./GroupCard";
+import { styles } from "@/src/css/group.styles";
+
+export default function GroupList({
+  groups,
+  fadeAnim,
+  slideAnim,
+  onSelectGroup,
+}: any) {
+  return (
+    <Animated.View
+      style={[
+        styles.groupsContainer,
+        { opacity: fadeAnim, transform: [{ translateY: slideAnim }] },
+      ]}
+    >
+      <FlatList
+        data={groups}
+        extraData={groups} // ⭐ THIS IS THE FIX
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <GroupCard group={item} onPress={() => onSelectGroup(item)} />
+        )}
+        showsVerticalScrollIndicator={false}
+      />
+    </Animated.View>
+  );
+}
