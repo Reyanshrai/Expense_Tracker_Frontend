e4e1bb797194733cce8a35fb2229b0e7635d7fcc
reyanshrai <reyanshrai05@gmail.com>
2026-03-03 09:43:08 +0530

    Refactor createGroup and listenUserGroups functions for improved participant management and real-time updates, including role and status enhancements for participants.

--- src/services/group.ts
+++ src/services/group.ts
@@ -1,60 +1,63 @@
+import { db } from "@/src/services/firebase";
+import { Participant } from "@/src/types/group";
+import { User } from "firebase/auth";
 import {
   addDoc,
   collection,
-  query,
-  where,
   onSnapshot,
+  query,
   Timestamp,
+  where,
 } from "firebase/firestore";
-import { db } from "@/src/services/firebase";
-import { User } from "firebase/auth";
-import { Participant } from "@/src/types/group";
-
 
 export const createGroup = async (
-  user: User, 
+  user: User,
   groupName: string,
-  memberEmails: string[] = []
+  memberEmails: string[] = [],
 ) => {
-
-  const participants : Participant[] = [
+  const participants: Participant[] = [
     {
       id: user.uid,
       email: user.email!,
       name: user.displayName ?? "You",
+      role: "admin",
+      status: "accepted",
     },
     ...memberEmails.map((email) => ({
+      id: null, // will be updated when they accept
       email,
-      name: email.split("@")[0], // temp name
+      name: email.split("@")[0],
+      role: "member" as const,
+      status: "pending" as const,
     })),
   ];
 
-  const participantIds = participants
-    .filter((p) => p.id)
-    .map((p) => p.id);
+  const participantIds = participants.filter((p) => p.id).map((p) => p.id);
 
   return await addDoc(collection(db, "groups"), {
     name: groupName,
-    totalSpent: 0, 
+    totalSpent: 0,
     createdBy: user.uid,
     participants,
     participantIds,
+    status: "active",
     createdAt: Timestamp.now(),
   });
 };
 
 // Listen user Groups in real-time
 
-export const listenUserGroups = (userId: string, callback: (groups: any[]) => void) => {
-    
+export const listenUserGroups = (
+  userId: string,
+  callback: (groups: any[]) => void,
+) => {
+  const q = query(
+    collection(db, "groups"),
+    where("participantIds", "array-contains", userId),
+  );
 
-    const q = query(
-        collection(db, "groups"), 
-        where("participantIds", "array-contains", userId)
-    );
-
-    return onSnapshot(q, (snapshot) => {
-        const groups = snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
-        callback(groups);
-    });
-}
+  return onSnapshot(q, (snapshot) => {
+    const groups = snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+    callback(groups);
+  });
+};
